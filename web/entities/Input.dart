a63ce5138db9660c205da2b5f0ba7f8ea0ca1be0
Mark Wilkins <contactme@markwilkins.co>
2021-03-18 22:54:43 -0400

    refactor

--- web/entities/Input.dart
+++ web/entities/Input.dart
@@ -38,78 +38,114 @@ class Input {
   Input(this._inputID, this._displayID) {
     _display = querySelector(_displayID);
 
+    _verifyHtmlIsCorrect();
+    _createChildHtml();
+    _registerChildElements();
+    _attachBaseEventListeners();
+  }
+
+  void _verifyHtmlIsCorrect() {
     if (_display == null) {
       throw Element_Not_Found_Exception(_displayID);
     }
+  }
 
+  void _createChildHtml() {
     _display.appendHtml('''
       <span id="terminal-prompt">&#65310;</span><span id="terminal-caret"></span>
     ''');
+  }
 
+  void _registerChildElements() {
     _prompt = _display.querySelector('#terminal-prompt');
     _caret = _display.querySelector('#terminal-caret');
+  }
 
+  void _attachBaseEventListeners() {
     _display.addEventListener(
         'focus', (event) => _display.classes.add('focused'));
 
     _display.addEventListener(
         'blur', (event) => _display.classes.remove('focused'));
 
-    _display.addEventListener('keydown', (event) {
-      var e = event as KeyboardEvent;
+    _display.addEventListener('keydown', handleKeyDown);
+  }
 
-      var isBackspace = e.keyCode == 8;
-      var isPreviousNodeTextNode = _caret.previousNode.nodeType == 3;
-      var isNextNodeTextNode = _caret.nextNode?.nodeType == 3;
-      var canBeBackspaced = isBackspace && isPreviousNodeTextNode;
-      if (canBeBackspaced) {
-        _caret.previousNode.remove();
-        return;
-      }
+  void handleBackspace() {
+    _caret.previousNode.remove();
+    return;
+  }
 
-      var isMaxLength = _display.text.length >= 300;
-      if (isMaxLength) return;
+  void handleAddCharacter(String key) {
+    _display.insertBefore(Text(key), _caret);
+    return;
+  }
 
-      var isValidCharacter =
-          REG_EXP_VALID_CHAR.hasMatch(String.fromCharCode(e.keyCode)) ||
-              REG_EXP_VALID_CHAR.hasMatch(e.key);
+  void handleLeftArrow(Node previousCharacter, Node selectedCharacter) {
+    _caret.previousNode?.remove();
+    _caret.firstChild?.remove();
+    if (selectedCharacter != null) {
+      _display.insertBefore(selectedCharacter, _caret.nextNode);
+    }
+    _caret.append(previousCharacter);
+    return;
+  }
 
-      if (isValidCharacter && e.key.length == 1) {
-        _display.insertBefore(Text(e.key), _caret);
-        return;
-      }
+  void handleRightArrow(Node nextCharacter, Node selectedCharacter) {
+    _caret.nextNode?.remove();
+    _caret.firstChild?.remove();
+    if (selectedCharacter != null) {
+      _display.insertBefore(selectedCharacter, _caret);
+    }
+    _caret.append(nextCharacter);
+    return;
+  }
 
-      var isLeftArrow = e.keyCode == 37;
-      var isRightArrow = e.keyCode == 39;
+  void handleKeyDown(event) {
+    var e = event as KeyboardEvent;
+    var isValidCharacter =
+        REG_EXP_VALID_CHAR.hasMatch(String.fromCharCode(e.keyCode)) ||
+            REG_EXP_VALID_CHAR.hasMatch(e.key);
 
-      var previousCharacter = _caret.previousNode?.clone(false);
-      var selectedCharacter = _caret.firstChild?.clone(false);
-      var nextCharacter = _caret.nextNode?.clone(false);
-      if (isLeftArrow && isPreviousNodeTextNode) {
-        _caret.previousNode?.remove();
-        _caret.firstChild?.remove();
-        if (selectedCharacter != null) {
-          _display.insertBefore(selectedCharacter, _caret.nextNode);
-        }
-        _caret.append(previousCharacter);
-        return;
-      }
+    // Checks for specific keys
+    var isEnter = e.keyCode == 13;
+    var isBackspace = e.keyCode == 8;
+    var isLeftArrow = e.keyCode == 37;
+    var isRightArrow = e.keyCode == 39;
 
-      if (isRightArrow && isNextNodeTextNode) {
-        _caret.nextNode?.remove();
-        _caret.firstChild?.remove();
-        if (selectedCharacter != null) {
-          _display.insertBefore(selectedCharacter, _caret);
-        }
-        _caret.append(nextCharacter);
-        return;
-      }
+    // Check for other relevent properties
+    var isPreviousNodeTextNode = _caret.previousNode.nodeType == 3;
+    var isNextNodeTextNode = _caret.nextNode?.nodeType == 3;
+    var isMaxLength = _display.text.length >= 300;
+    var isSingleCharacter = e.key.length == 1;
 
-      var isEnter = e.keyCode == 13;
-      if (isEnter) {
-        _handleSubmit();
-      }
-    });
+    // Characters in and surrounding the carret
+    var previousCharacter = _caret.previousNode?.clone(false);
+    var selectedCharacter = _caret.firstChild?.clone(false);
+    var nextCharacter = _caret.nextNode?.clone(false);
+
+    // computed checks
+    var canBeBackspaced = isBackspace && isPreviousNodeTextNode;
+
+    if (canBeBackspaced) handleBackspace();
+
+    if (isMaxLength) return;
+
+    if (isValidCharacter && isSingleCharacter) {
+      handleAddCharacter(e.key);
+    }
+
+    if (isLeftArrow && isPreviousNodeTextNode) {
+      handleLeftArrow(previousCharacter, selectedCharacter);
+    }
+
+    if (isRightArrow && isNextNodeTextNode) {
+      handleRightArrow(nextCharacter, selectedCharacter);
+    }
+
+    if (isEnter) {
+      _handleSubmit();
+    }
   }
 
   void _handleSubmit() {
